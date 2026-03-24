7712a8cdca42d13f0dd912ce7e816d2768551d11
Marco Cerilli <cerillimarco15@gmail.com>
2026-03-24 18:24:27 +0100

    corretto robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,21 +1,21 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: '*',
-        allow: '/',
+        userAgent: "*",
+        allow: ["/", "/_next/static/images/"],
         // Blocchiamo l'indicizzazione delle cartelle di sviluppo e API
         disallow: [
-          '/ai/',           // I tuoi flussi Genkit e file dev
-          '/api/',          // Le rotte nodemailer e contatti
-          '/coming-soon',   // Pagina temporanea di cantiere
-          '/private/',      // Eventuali aree riservate
+          "/ai/", // I tuoi flussi Genkit e file dev
+          "/api/", // Le rotte nodemailer e contatti
+          "/coming-soon", // Pagina temporanea di cantiere
+          "/private/", // Eventuali aree riservate
         ],
       },
     ],
     // Specifichiamo l'URL assoluto della sitemap per Search Console
-    sitemap: 'https://www.mavearredamenti.it/sitemap.xml',
+    sitemap: "https://www.mavearredamenti.it/sitemap.xml",
   };
 }
