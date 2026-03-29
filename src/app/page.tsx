066a5ecd42d1992807ef25c597176d2d5da807b5
Marco Cerilli <cerillimarco15@gmail.com>
2026-03-29 13:24:35 +0200

    schiarita foto home mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
               alt={heroMobile.description}
               fill
               priority
-              className="object-cover object-top brightness-[0.75] contrast-[1.05]"
+              className="object-cover object-top brightness-[1.25] contrast-[1.0]"
               unoptimized
             />
             <div
@@ -93,7 +93,7 @@ export default function Home() {
                 backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
               }}
             />
-            <div className="absolute inset-0 bg-black/35" />
+            <div className="absolute inset-0 bg-black/20" />
           </div>
         )}
 
